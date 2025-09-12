8a5cb9b07abc33952543521b912a05c9eaa25689
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-12 11:31:56 +0200

    DEV - Update kubejs

--- astro-rise/src/kubejs/server_scripts/commands/gm.js
+++ astro-rise/src/kubejs/server_scripts/commands/gm.js
@@ -3,10 +3,10 @@ ServerEvents.commandRegistry(event => {
 
 	const { commands: Commands, arguments: Arguments } = event
 
-	const arg = Commands.argument('gamemode', Arguments.GAMEMODE.create(event))
+	const arg = Commands.argument('gamemode', Arguments.STRING.create(event))
 
 	const cmd = Commands.literal('gm').then(arg).executes(ctx => {
-		let gamemode = Arguments.GAMEMODE.getResult(ctx, 'gamemode')
+		let gamemode = Arguments.STRING.getResult(ctx, 'gamemode')
 		ctx.source.server.runCommand(`gamemode ${gamemode} ${ctx.source.player.name.string}`)
 		return 1
 	})
