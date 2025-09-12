f1669c9ce2d107e5b8b1cc8a6340a996a8c26c49
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-12 09:44:52 +0200

    Update KubeJS

---
+++ astro-rise/src/kubejs/server_scripts/mechanics/space_food.js
@@ -0,0 +1,42 @@
+
+// ServerEvents.recipes(event => {
+// 	event.remove({ output: "sophisticatedbackpacks:auto_feeder_upgrade" })
+// })
+
+PlayerEvents.changedDimension(event => {
+
+	let player = event.player
+	let inventory = player.inventory
+
+	if (!event.to.location().startsWith("ad_astra:")) return
+
+	for (let i = 0; i < inventory.size(); i++)
+	{
+		let stack = inventory.get(i)
+
+		if (stack.isEmpty()) continue
+
+		if (stack.item.foodProperties) {
+			if (stack.id === "astro_rise:space_food") continue
+			inventory.set(i, Item.of("minecraft:rotten_flesh", stack.count))
+			player.tell(`The food ${stack.displayName} decomposed in the hostile atmosphere!`)
+		}
+	}
+
+})
+
+PlayerEvents.inventoryChanged(event => {
+
+  	let player = event.player
+  	let stack = event.item
+
+	if (!player.level.dimension.location().startsWith("ad_astra:")) return
+
+  	if (!stack.item.foodProperties) return
+
+  	if (stack.id === "astro_rise:space_food") return
+
+	player.inventory.setItem(event.slot, Item.of("minecraft:rotten_flesh", stack.count))
+	player.tell(`The food ${stack.displayName} decomposed in the hostile atmosphere!`)
+
+})
