60306f1e2d1100307a117becc6e8b4d0f98f4ca1
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-12 12:10:07 +0200

    DEV - Update kubejs

--- astro-rise/src/kubejs/server_scripts/recipes/tconstruct/melting.js
+++ astro-rise/src/kubejs/server_scripts/recipes/tconstruct/melting.js
@@ -2,10 +2,10 @@ ServerEvents.recipes(event => {
 
 	// molten_necrotic_bone
 
-	event.recipes.tconstruct.melting({
+	event.recipes.tconstruct.melting(
 	    { item: 'tconstruct:necrotic_bone' },
 	    { fluid: 'astro_rise:molten_necrotic_bone', amount: 100 },
 	    950
-	})
+	)
 
 })
