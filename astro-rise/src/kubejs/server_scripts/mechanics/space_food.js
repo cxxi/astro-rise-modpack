08ec5806b48e7420ef2537c644769ebdfec16dbf
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-12 11:42:32 +0200

    DEV - Update kubejs

--- astro-rise/src/kubejs/server_scripts/mechanics/space_food.js
+++ astro-rise/src/kubejs/server_scripts/mechanics/space_food.js
@@ -3,40 +3,40 @@
 // 	event.remove({ output: "sophisticatedbackpacks:auto_feeder_upgrade" })
 // })
 
-PlayerEvents.playerChangedDimension(event => {
+// PlayerEvents.playerChangedDimension(event => {
 
-	let player = event.player
-	let inventory = player.inventory
+// 	let player = event.player
+// 	let inventory = player.inventory
 
-	if (!event.to.toString().startsWith("ad_astra:")) return
+// 	if (!event.to.toString().startsWith("ad_astra:")) return
 
-	for (let i = 0; i < inventory.size(); i++)
-	{
-		let stack = inventory.get(i)
+// 	for (let i = 0; i < inventory.size(); i++)
+// 	{
+// 		let stack = inventory.get(i)
 
-		if (stack.isEmpty()) continue
+// 		if (stack.isEmpty()) continue
 
-		if (stack.item.foodProperties) {
-			if (stack.id === "astro_rise:space_food") continue
-			inventory.set(i, Item.of("minecraft:rotten_flesh", stack.count))
-			player.tell(`The food ${stack.displayName} decomposed in the hostile atmosphere!`)
-		}
-	}
+// 		if (stack.item.foodProperties) {
+// 			if (stack.id === "astro_rise:space_food") continue
+// 			inventory.set(i, Item.of("minecraft:rotten_flesh", stack.count))
+// 			player.tell(`The food ${stack.displayName} decomposed in the hostile atmosphere!`)
+// 		}
+// 	}
 
-})
+// })
 
-PlayerEvents.inventoryChanged(event => {
+// PlayerEvents.inventoryChanged(event => {
 
-  	let player = event.player
-  	let stack = event.item
+//   	let player = event.player
+//   	let stack = event.item
 
-	if (!player.level.dimension.toString().startsWith("ad_astra:")) return
+// 	if (!player.level.dimension.toString().startsWith("ad_astra:")) return
 
-  	if (!stack.item.foodProperties) return
+//   	if (!stack.item.foodProperties) return
 
-  	if (stack.id === "astro_rise:space_food") return
+//   	if (stack.id === "astro_rise:space_food") return
 
-	player.inventory.setItem(event.slot, Item.of("minecraft:rotten_flesh", stack.count))
-	player.tell(`The food ${stack.displayName} decomposed in the hostile atmosphere!`)
+// 	player.inventory.setItem(event.slot, Item.of("minecraft:rotten_flesh", stack.count))
+// 	player.tell(`The food ${stack.displayName} decomposed in the hostile atmosphere!`)
 
-})
+// })
