5cc52456c9af9dc27a2b4b0e131dbec8386b0619
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-13 04:16:34 +0200

    DEV - make installer executable

---
+++ astro-rise/src/kubejs/server_scripts/recipes/structurecompass/tools.js
@@ -0,0 +1,20 @@
+ServerEvents.recipes(event => {
+
+	// structure_compass
+
+	event.remove({ id: 'structurecompass:structure_compass' })
+
+	event.shaped('structurecompass:structure_compass', [
+	    'ABA', 
+	    'CDE',
+	    'AFA'  
+	], {
+	    A: 'enderio:broken_spawner', 
+	    B: 'minecraft:rib_armor_trim_smithing_template',
+	    C: 'minecraft:eye_armor_trim_smithing_template',
+	    D: 'minecraft:compass',
+	    E: 'minecraft:spire_armor_trim_smithing_template',
+	    F: 'minecraft:tide_armor_trim_smithing_template'
+	})
+
+})
