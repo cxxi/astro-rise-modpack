c84aaff00edf4112bc321a36f90598c19d68e9e0
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-14 00:51:37 +0200

    DEV - Update kubejs

--- astro-rise/src/kubejs/server_scripts/tags/items/elytra.js
+++ astro-rise/src/kubejs/server_scripts/tags/items/elytra.js
@@ -7,15 +7,15 @@
 // })
 
 ServerEvents.highPriorityData(event => {
-  event.addJson('elytraslot:tags/item/elytra', {
-    replace: false,
-    values: [
-      "minecraft:elytra",
-      "alexsmobs:tarantula_hawk_elytra",
-      "deeperdarker:soul_elytra",
-      "mekanism:hdpe_elytra",
-      "tconstruct:slime_chestplate"
-    ]
-  })
+  // event.addJson('elytraslot:tags/item/elytra', {
+  //   replace: false,
+  //   values: [
+  //     "minecraft:elytra",
+  //     "alexsmobs:tarantula_hawk_elytra",
+  //     "deeperdarker:soul_elytra",
+  //     "mekanism:hdpe_elytra",
+  //     "tconstruct:slime_chestplate"
+  //   ]
+  // })
   console.info("[KubeJS] ElytraSlot patch loaded ✅")
 })
