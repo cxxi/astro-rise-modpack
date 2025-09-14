825d11964cd48ad463dcd1de0655d4772d7cea35
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-14 13:41:24 +0200

    DEV - Update kubejs

--- astro-rise/src/kubejs/server_scripts/recipes/create/filling.js
+++ astro-rise/src/kubejs/server_scripts/recipes/create/filling.js
@@ -18,9 +18,4 @@ ServerEvents.recipes(event => {
 		]
 	})
 
-	// event.recipes.create.filling('astro_rise:sculk_mechanism', [
-	// 	'astro_rise:liquid_sculk', 
-	// 	'create:precision_mechanism'
-	// ])
-
 })
