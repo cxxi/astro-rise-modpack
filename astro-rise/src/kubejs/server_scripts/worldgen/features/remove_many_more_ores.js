da7d10185c67c92eb21eba62c14ead41211d0e31
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-16 08:47:29 +0200

    DEV - remove news ore generation

--- astro-rise/src/kubejs/server_scripts/worldgen/features/remove_many_more_ores.js
+++ astro-rise/src/kubejs/server_scripts/worldgen/features/remove_many_more_ores.js
@@ -1,23 +1,23 @@
-ServerEvents.highPriorityData(event => {
+// ServerEvents.highPriorityData(event => {
 
-    const ores = [
-        'adamantite',
-        'cobalt',
-        'infernal',
-        'lead',
-        'mythril',
-        'orichalcum',
-        'palladium',
-        'platinum',
-        'silver',
-        'tin',
-        'titanium',
-        'tungsten'
-    ]
+//     const ores = [
+//         'adamantite',
+//         'cobalt',
+//         'infernal',
+//         'lead',
+//         'mythril',
+//         'orichalcum',
+//         'palladium',
+//         'platinum',
+//         'silver',
+//         'tin',
+//         'titanium',
+//         'tungsten'
+//     ]
     
-    ores.forEach(ore => {
-        event.remove(`many_more_ores_and_crafts:worldgen/placed_feature/${ore}_ore_placed`)
-        event.remove(`many_more_ores_and_crafts:worldgen/configured_feature/${ore}_ore`)
-    })
+//     ores.forEach(ore => {
+//         event.remove(`many_more_ores_and_crafts:worldgen/placed_feature/${ore}_ore_placed`)
+//         event.remove(`many_more_ores_and_crafts:worldgen/configured_feature/${ore}_ore`)
+//     })
 
-})
+// })
