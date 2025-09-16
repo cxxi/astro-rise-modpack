66c19f9d4b57f8bcc459467e5f477e7432b8bb43
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-16 10:01:51 +0200

    DEV - remove news ore generation

--- astro-rise/src/kubejs/server_scripts/worldgen/structures/final_castle.js
+++ astro-rise/src/kubejs/server_scripts/worldgen/structures/final_castle.js
@@ -1,7 +1,18 @@
-WorldGenEvents.highPriorityData(event => {
+ServerEvents.highPriorityData(event => {
 
-    event.modify('twilightforest:final_plateau', biome => {
-        biome.structures = biome.structures.filter(struct => struct.structure != 'twilightforest:final_castle')
-    })
+    let finalPlateau = event.get('twilightforest:final_plateau')
 
+    if (finalPlateau && finalPlateau.structures) {
+
+        finalPlateau.structures = finalPlateau.structures.filter(s => s.structure != 'twilightforest:final_castle')
+
+        // finalPlateau.structures.push({
+        //     structure: 'my_mod:my_end_portal_castle',
+        //     spacing: 1,
+        //     separation: 1,
+        //     salt: 12345
+        // })
+
+        event.add('twilightforest:final_plateau', finalPlateau)
+    }
 })
