41e22065394a4593bd533e41400e80155c0248c6
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-20 00:50:57 +0200

    DEV - work on recipes

--- astro-rise/src/kubejs/server_scripts/recipes/enderio/alloy_smelting.js
+++ astro-rise/src/kubejs/server_scripts/recipes/enderio/alloy_smelting.js
@@ -1,21 +1,27 @@
+// ServerEvents.recipes(event => {
+
+// 	// cake_base
+
+// 	event.remove({ id: 'enderio:alloy_smelting/cake_base' })
+
+// 	// dark_steel_ingot
+
+// 	event.remove({ id: "enderio:alloy_smelting/dark_steel_ingot" })
+
+// 	event.recipes.enderio.alloy_smelting(
+// 		'enderio:dark_steel_ingot', 
+// 		[
+// 			'mekanism:ingot_steel',
+// 			'bigreactors:graphite_dust',
+// 			'mekanism:block_refined_obsidian'
+// 		],
+// 		6400
+// 	)
+
+// })
+
 ServerEvents.recipes(event => {
 
-	// cake_base
-
-	event.remove({ id: 'enderio:alloy_smelting/cake_base' })
-
-	// dark_steel_ingot
-
-	event.remove({ id: "enderio:alloy_smelting/dark_steel_ingot" })
-
-	event.recipes.enderio.alloy_smelting(
-		'enderio:dark_steel_ingot', 
-		[
-			'mekanism:ingot_steel',
-			'bigreactors:graphite_dust',
-			'mekanism:block_refined_obsidian'
-		],
-		6400
-	)
+  event.remove({ type: 'enderio:alloy_smelting' })
 
 })
