5e41d4f7912ff6ff648e3e2cb3cdcbf05af50c0a
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-20 13:18:17 +0200

    DEV - Ruber recipe chain

--- astro-rise/src/kubejs/server_scripts/recipes/thermal/misc.js
+++ astro-rise/src/kubejs/server_scripts/recipes/thermal/misc.js
@@ -8,59 +8,59 @@ ServerEvents.recipes(event => {
 
     // kelp to rubber
 
-    const targetMods = ['create', 'petrolsparts', 'createdieselgenerators']
-	const skipIds = ['createdieselgenerators:crafting/kelp_handle']
-	const from = 'minecraft:dried_kelp'
-	const to = 'thermal:cured_rubber'
+    // const targetMods = ['create', 'petrolsparts', 'createdieselgenerators']
+	// const skipIds = ['createdieselgenerators:crafting/kelp_handle']
+	// const from = 'minecraft:dried_kelp'
+	// const to = 'thermal:cured_rubber'
 
-	event.forEachRecipe({}, recipe => {
+	// event.forEachRecipe({}, recipe => {
 
-	    const id = recipe.getId()
-	    if (!id) return
+	//     const id = recipe.getId()
+	//     if (!id) return
 
-	    const mod = id.split(':')[0]
-	    if (!targetMods.includes(mod)) return
-	    if (skipIds.includes(id)) return
+	//     const mod = id.split(':')[0]
+	//     if (!targetMods.includes(mod)) return
+	//     if (skipIds.includes(id)) return
 
-	    let json = recipe.json
-	    let changed = false
+	//     let json = recipe.json
+	//     let changed = false
 
-	    if (json.key) {
-	    	for (const symbol in json.key) {
-	    		const ing = json.key[symbol]
-	    		if (ing.item === from) {
-	    			ing.item = to
-	    			changed = true
-	    		}
-	    	}
-	    }
+	//     if (json.key) {
+	//     	for (const symbol in json.key) {
+	//     		const ing = json.key[symbol]
+	//     		if (ing.item === from) {
+	//     			ing.item = to
+	//     			changed = true
+	//     		}
+	//     	}
+	//     }
 
-	    if (json.ingredients) {
-	    	json.ingredients.forEach(ing => {
-	    		if (ing.item === from) {
-	    			ing.item = to
-	    			changed = true
-	    		}
-	    	})
-	    }
+	//     if (json.ingredients) {
+	//     	json.ingredients.forEach(ing => {
+	//     		if (ing.item === from) {
+	//     			ing.item = to
+	//     			changed = true
+	//     		}
+	//     	})
+	//     }
 
-	    if (json.input && json.input.item === from) {
-	    	json.input.item = to
-	    	changed = true
-	    }
+	//     if (json.input && json.input.item === from) {
+	//     	json.input.item = to
+	//     	changed = true
+	//     }
 
-	    if (json.inputs) {
-	    	json.inputs.forEach(ing => {
-	    		if (ing.item === from) {
-	    			ing.item = to
-	    			changed = true
-	    		}
-	    	})
-	    }
+	//     if (json.inputs) {
+	//     	json.inputs.forEach(ing => {
+	//     		if (ing.item === from) {
+	//     			ing.item = to
+	//     			changed = true
+	//     		}
+	//     	})
+	//     }
 
-	    if (changed) {
-	    	event.custom(json).id(id)
-	    }
-	})
+	//     if (changed) {
+	//     	event.custom(json).id(id)
+	//     }
+	// })
 
 })
