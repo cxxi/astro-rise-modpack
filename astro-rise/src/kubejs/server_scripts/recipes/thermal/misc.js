1f29d00c3479262d43dba058009e7beaf715d033
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-20 12:45:57 +0200

    DEV - Ruber recipe chain

--- astro-rise/src/kubejs/server_scripts/recipes/thermal/misc.js
+++ astro-rise/src/kubejs/server_scripts/recipes/thermal/misc.js
@@ -2,22 +2,45 @@ ServerEvents.recipes(event => {
 
     // rubber
 
+	event.remove({ id: 'thermal:rubber_3' })
     event.remove({ id: 'thermal:rubber_from_vine' })
 	event.remove({ id: 'thermal:rubber_from_dandelion' })
 
-    // event.custom({
-    //     type: 'treetap:tap_extract',
-    //     log: {
-    //         item: 'thermal:rubberwood_log'
-    //     },
-    //     processing_time: 1200,
-    //     result: {
-    //         item: 'thermal:resin_bucket'
-    //     },
-    //     collect_bucket: true,
-    //     display_fluid: {
-    //         fluid: 'thermal:resin'
-    //     }
-    // })
+    // kelp to rubber
+
+    const targetMods = ['create', 'petrolsparts', 'createdieselgenerators']
+	const skipIds = ['createdieselgenerators:crafting/kelp_handle']
+
+	event.forEachRecipe({ }, recipe => {
+
+		if (!targetMods.includes(recipe.mod)) return
+		if (skipIds.includes(recipe.getId())) return
+
+		let changed = false
+		let json = recipe.json
+
+		if (json.ingredients) {
+			json.ingredients.forEach(ing => {
+				if (ing.item === 'minecraft:dried_kelp') {
+					ing.item = 'thermal:cured_rubber'
+					changed = true
+				}
+			})
+		}
+
+		if (json.key) {
+			Object.keys(json.key).forEach(k => {
+				let ing = json.key[k]
+				if (ing.item === 'minecraft:dried_kelp') {
+					ing.item = 'thermal:cured_rubber'
+					changed = true
+				}
+			})
+		}
+
+		if (changed) {
+			event.custom(json).id(recipe.getId())
+		}
+	})
 
 })
