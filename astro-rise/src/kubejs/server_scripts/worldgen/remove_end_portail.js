9206eed091020ef25de8c1010782cf36cfb85b3a
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-20 14:37:30 +0200

    DEV - Overwiew ores

--- astro-rise/src/kubejs/server_scripts/worldgen/remove_end_portail.js
+++
@@ -1,7 +0,0 @@
-WorldgenEvents.structure(event => {
-
-	if (event.structure.id === 'minecraft:stronghold') {
-		event.blocks.replace('minecraft:end_portal_frame', 'minecraft:stone_bricks')
-	}
-	
-})
