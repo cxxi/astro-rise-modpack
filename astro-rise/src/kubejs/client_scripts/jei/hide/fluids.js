51474975d629e6a0f48422cb8fc89180d93cd0ec
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-21 17:17:50 +0200

    DEV - Work of jules

--- astro-rise/src/kubejs/client_scripts/jei/hide/fluids.js
+++ astro-rise/src/kubejs/client_scripts/jei/hide/fluids.js
@@ -2,8 +2,8 @@ JEIEvents.hideFluids(event => {
 
 	event.hide('create_mechanical_chicken:seed_oil')
 
-	event.hide('bigreactors:yellorium')
-	event.hide('bigreactors:yellorium_flowing')
+	// event.hide('bigreactors:yellorium')
+	// event.hide('bigreactors:yellorium_flowing')
 
 	event.hide('ad_astra:oil')
 	
