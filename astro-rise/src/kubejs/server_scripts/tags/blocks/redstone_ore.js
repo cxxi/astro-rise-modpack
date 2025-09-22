877713538a129953338abde391825391340977c2
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-22 16:45:55 +0200

    DEV - Work of jules

---
+++ astro-rise/src/kubejs/server_scripts/tags/blocks/redstone_ore.js
@@ -0,0 +1,8 @@
+ServerEvents.tags('block', event => {
+
+  event.remove('forge:ores/redstone', [
+    'deeperdarker:sculk_stone_redstone_ore',
+    'deeperdarker:gloomslate_redstone_ore'
+  ])
+  
+})
