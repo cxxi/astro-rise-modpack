6246a20e2fb697e7e2d59ffd78d03bb0737f6817
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-23 19:54:13 +0200

    Adding all vein/drilling supports

--- astro-rise/src/kubejs/server_scripts/tags/worldgen/biomes/ad_astra.js
+++ astro-rise/src/kubejs/server_scripts/tags/worldgen/biomes/ad_astra.js
@@ -2,4 +2,21 @@ ServerEvents.tags('worldgen/biome', event => {
 
 	event.remove('ad_astra:has_structure/oil_well')
 
+	event.add('ad_astra:has_moon_vein', [
+	    'ad_astra:lunar_wastelands'
+	])
+
+	event.add('ad_astra:has_mars_vein', [
+	    'ad_astra:martian_wastelands', 
+	    'ad_astra:martian_canyon_creek'
+	])
+
+	event.add('ad_astra:has_venus_vein', [
+	    'ad_astra:venus_wastelands',
+	   'ad_astra:infernal_venus_barrens'
+	])
+
+	event.add('ad_astra:has_mercury_vein', [
+	   'ad_astra:mercury_deltas'
+	])
 })
