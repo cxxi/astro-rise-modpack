fd5f5c3680323197c86ed06e48588fbc31f07498
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-24 00:26:03 +0200

    Work on ores processing

--- astro-rise/src/kubejs/startup_scripts/blocks/mercury_aerolyte_ore.js
+++ astro-rise/src/kubejs/startup_scripts/blocks/mercury_aerolyte_ore.js
@@ -12,24 +12,23 @@ StartupEvents.registry('block', event => {
 	    .tagBlock("minecraft:needs_iron_tool")           
 	    .tagBlock("forge:ores")                      
 	    .tagBlock("forge:ores/mercury_aerolyte")
-	    .drop("astro_rise:raw_aerolyte")
-	    // .setLootTableJson({
-	    // 	type: "minecraft:block",
-	    // 	pools: [
-	    // 		{
-	    // 			rolls: 1,
-	    // 			entries: [
-	    // 				{
-    	// 					type: "minecraft:item",
-    	// 					name: "astro_rise:raw_aerolyte"
-	    // 				}
-	    // 			],
-	    // 			conditions: [
-    	// 				{
-    	// 					condition: "minecraft:survives_explosion"
-    	// 				}
-	    // 			]
-	    // 		}
-	    // 	]
-	    // })
+	    .setLootTableJson({
+	    	"type": "minecraft:block",
+	    	"pools": [
+	    		{
+	    			"rolls": 1,
+	    			"entries": [
+	    				{
+    						"type": "minecraft:item",
+    						"name": "astro_rise:raw_aerolyte"
+	    				}
+	    			],
+	    			"conditions": [
+    					{
+    						"condition": "minecraft:survives_explosion"
+    					}
+	    			]
+	    		}
+	    	]
+	    })
 })
