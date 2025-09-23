9aec2e3b048ff912d319e496b7a4617bfe28a688
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-24 01:01:49 +0200

    Work on ores processing

--- astro-rise/src/kubejs/startup_scripts/items/ingot_aerolyte.js
+++ astro-rise/src/kubejs/startup_scripts/items/ingot_aerolyte.js
@@ -2,6 +2,6 @@ StartupEvents.registry('item', event => {
 
 	event
 		.create('astro_rise:ingot_aerolyte')
-		.texture('astro_rise:item/ingot_aerolyte')
+		.texture('astro_rise:item/aerolyte_ingot')
 		.displayName('Aerolyte Ingot')
 })
