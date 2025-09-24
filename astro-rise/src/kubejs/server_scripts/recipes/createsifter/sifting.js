522387ad3f9ba6b1f179aa6d187158381b39f2e3
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-24 18:15:24 +0200

    Work on ores processing

--- astro-rise/src/kubejs/server_scripts/recipes/createsifter/sifting.js
+++ astro-rise/src/kubejs/server_scripts/recipes/createsifter/sifting.js
@@ -27,7 +27,7 @@ ServerEvents.recipes(event => {
 	event.recipes.createsifter.sifting([
 		Item.of('minecraft:flint').withChance(0.05),
 		Item.of('minecraft:bone_meal').withChance(0.05),
-		Item.of('astro_rise:andesite_nugget').withChance(0.1)
+		Item.of('astro_rise:andesite_nugget').withChance(0.10)
 	], ['minecraft:gravel','createsifter:zinc_mesh'])
 
 	// brass mesh
@@ -63,23 +63,49 @@ ServerEvents.recipes(event => {
 	event.remove({ id: 'createsifter:sifting/soul_sand_advanced_brass_mesh' })
 
 	event.recipes.createsifter.sifting([
-		Item.of('minecraft:gold_nugget').withChance(0.05)
+		Item.of('mekanism:dust_iron').withChance(0.05),
+		Item.of('mekanism:dust_copper').withChance(0.05),
+		Item.of('astro_rise:dust_zinc').withChance(0.05),
+		Item.of('mekanism:dust_tin').withChance(0.04),
+		Item.of('astro_rise:dust_aluminum').withChance(0.04),
+		Item.of('mekanism:dust_lead').withChance(0.04),
+		Item.of('thermal:nickel_dust').withChance(0.04),
+		Item.of('thermal:silver_dust').withChance(0.04),
+		Item.of('mekanism:dust_osmium').withChance(0.03),
+		Item.of('astro_rise:dust_platinum').withChance(0.01)
 	], ['minecraft:gravel','createsifter:advanced_brass_mesh'])
 
 	event.recipes.createsifter.sifting([
-		Item.of('minecraft:gold_nugget').withChance(0.05)
+		Item.of('mekanism:dust_gold').withChance(0.05),
+		Item.of('mekanism:dust_lapis_lazuli').withChance(0.05),
+		Item.of('minecraft:redstone').withChance(0.05),
+		Item.of('mekanism:dust_emerald').withChance(0.03),
+		Item.of('thermal:ruby_dust').withChance(0.03),
+		Item.of('thermal:sapphire_dust').withChance(0.03),
+		Item.of('createaddition:diamond_grit').withChance(0.01)
 	], ['minecraft:sand','createsifter:advanced_brass_mesh'])
 
 	event.recipes.createsifter.sifting([
-		Item.of('minecraft:gold_nugget').withChance(0.05)
+		Item.of('create:experience_nugget').withChance(0.10),
+		Item.of('thermal:cinnabar_dust').withChance(0.03),
+		Item.of('minecraft:ghast_tear').withChance(0.03),
+		Item.of('minecraft:bone_meal').withChance(0.05),
+		Item.of('mekanism:dust_coal').withChance(0.05)
 	], ['minecraft:soul_sand','createsifter:advanced_brass_mesh'])
 
 	event.recipes.createsifter.sifting([
-		Item.of('minecraft:gold_nugget').withChance(0.05)
+		Item.of('mekanism:dust_quartz').withChance(0.05),
+		Item.of('minecraft:glowstone_dust').withChance(0.05),
+		Item.of('minecraft:nether_wart').withChance(0.03),
+		Item.of('minecraft:blaze_powder').withChance(0.03),
+		Item.of('enderio:withering_powder').withChance(0.01)
 	], ['astro_rise:crushed_netherrack','createsifter:advanced_brass_mesh'])
 
 	event.recipes.createsifter.sifting([
-		Item.of('minecraft:gold_nugget').withChance(0.05)
+		Item.of('platinum_mod:ender_tear').withChance(0.03),
+		Item.of('astro_rise:shulker_scute').withChance(0.05),
+		Item.of('tconstruct:dragon_scale').withChance(0.05)
+		// Item.of('minecraft:gold_nugget').withChance(0.05)
 	], ['createsifter:crushed_end_stone','createsifter:advanced_brass_mesh'])
 
 })
