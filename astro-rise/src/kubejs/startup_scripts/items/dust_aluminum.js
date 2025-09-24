60f52bd5be3e875925c7a88d58ddf30237293f06
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-24 12:26:10 +0200

    Work on ores processing

---
+++ astro-rise/src/kubejs/startup_scripts/items/dust_aluminum.js
@@ -0,0 +1,8 @@
+StartupEvents.registry('item', event => {
+
+	event
+		.create('astro_rise:dust_aluminum')
+		.texture('astro_rise:item/dust_aluminum')
+		.displayName('Aluminum Dust')
+
+})
---
+++ astro-rise/src/kubejs/startup_scripts/items/dust_platinum.js
@@ -0,0 +1,8 @@
+StartupEvents.registry('item', event => {
+
+	event
+		.create('astro_rise:dust_platinum')
+		.texture('astro_rise:item/dust_platinum')
+		.displayName('Platinum Dust')
+
+})
