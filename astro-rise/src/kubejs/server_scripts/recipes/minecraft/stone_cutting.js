b06e69e16b13a7e6ed54e85b91c19f36586bd7b9
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-25 12:59:41 +0200

    Work on ores processing

---
+++ astro-rise/src/kubejs/server_scripts/recipes/minecraft/stone_cutting.js
@@ -0,0 +1,23 @@
+ServerEvents.recipes(event => {
+
+	event.stonecutting(
+	    Item.of('createdieselgenerators:mold', {Mold: 'createdieselgenerators:bowl' }),
+	    'simplemetals_aluminum:aluminum_sheet'
+	)
+
+	event.stonecutting(
+	    Item.of('createdieselgenerators:mold', {Mold: 'createdieselgenerators:wavy' }),
+	    'simplemetals_aluminum:aluminum_sheet'
+  	)
+
+	event.stonecutting(
+	    Item.of('createdieselgenerators:mold', {Mold: 'createdieselgenerators:chain' }),
+	    'simplemetals_aluminum:aluminum_sheet'
+  	)
+
+	event.stonecutting(
+	    Item.of('createdieselgenerators:mold', {Mold: 'createdieselgenerators:bar' }),
+	    'simplemetals_aluminum:aluminum_sheet'
+  	)
+
+})
