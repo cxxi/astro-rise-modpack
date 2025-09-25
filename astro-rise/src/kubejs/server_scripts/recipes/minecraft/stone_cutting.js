8aa0c657d0aeb6f18afde46f2c3a08a7dd273e31
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-25 13:29:22 +0200

    Work on ores processing

--- astro-rise/src/kubejs/server_scripts/recipes/minecraft/stone_cutting.js
+++ astro-rise/src/kubejs/server_scripts/recipes/minecraft/stone_cutting.js
@@ -1,36 +1,34 @@
 ServerEvents.recipes(event => {
 
-	event.stonecutting(
-	    Item.of('createdieselgenerators:mold', 1, { Mold: 'createdieselgenerators:bowl' }).strongNBT(),
-	    'simplemetals_aluminum:aluminum_sheet'
-	).id('astro_rise:stonecut/mold_bowl')
+	// event.stonecutting(
+	//     Item.of('createdieselgenerators:mold', 1, { Mold: 'createdieselgenerators:bowl' }).strongNBT(),
+	//     'simplemetals_aluminum:aluminum_sheet'
+	// ).id('astro_rise:stonecut/mold_bowl')
 
-	event.stonecutting(
-	    Item.of('createdieselgenerators:mold').withNBT({ Mold: 'createdieselgenerators:bar' }),
-	    'simplemetals_aluminum:aluminum_sheet'
-  	).id('astro_rise:stonecut/mold_wavy')
+	// event.stonecutting(
+	//     Item.of('createdieselgenerators:mold').withNBT({ Mold: 'createdieselgenerators:bar' }),
+	//     'simplemetals_aluminum:aluminum_sheet'
+  	// ).id('astro_rise:stonecut/mold_wavy')
 
-	event.stonecutting(
-	    Item.of('createdieselgenerators:mold', 1, { Mold: 'createdieselgenerators:chain' }).weakNBT(),
-	    'simplemetals_aluminum:aluminum_sheet'
-  	).id('astro_rise:stonecut/mold_chain')
+	// event.stonecutting(
+	//     Item.of('createdieselgenerators:mold', 1, { Mold: 'createdieselgenerators:chain' }).weakNBT(),
+	//     'simplemetals_aluminum:aluminum_sheet'
+  	// ).id('astro_rise:stonecut/mold_chain')
 
-	event.stonecutting(
-	    Item.withNBT('createdieselgenerators:mold', { Mold: 'createdieselgenerators:bar' }),
-	    'simplemetals_aluminum:aluminum_sheet'
-  	).id('astro_rise:stonecut/mold_bar')
+	// event.stonecutting(
+	//     Item.withNBT('createdieselgenerators:mold', { Mold: 'createdieselgenerators:bar' }),
+	//     'simplemetals_aluminum:aluminum_sheet'
+  	// ).id('astro_rise:stonecut/mold_bar')
 
   	event.custom({
-		type: "minecraft:stonecutting",
+		type: 'minecraft:stonecutting',
 		ingredient: {
-			item: "minecraft:mangrove_log"
+			item: 'simplemetals_aluminum:aluminum_sheet'
 		},
 		result: {
-			item: {
-				id: 'createdieselgenerators:mold',
-				components: {
-					Mold: 'createdieselgenerators:bar' 
-				}
+			id: 'createdieselgenerators:mold',
+			components: {
+				Mold: 'createdieselgenerators:bar' 
 			}
 		}
   	})
