d6d4f8b0ca331b2d08b14384e10ab07734e88ac6
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-25 11:55:16 +0200

    Work on ores processing

--- astro-rise/src/kubejs/server_scripts/recipes/create/sequenced_assembling.js
+++ astro-rise/src/kubejs/server_scripts/recipes/create/sequenced_assembling.js
@@ -1,12 +1,12 @@
 ServerEvents.recipes(event => {
 
 	event.recipes.create.sequenced_assembly([
-		Item.of('astro_rise:steel_mechanism').withChance(120.0),
-		Item.of('create:powdered_obsidian').withChance(6.0),
-		Item.of('thermal:invar_nugget').withChance(8.0),
-		Item.of('mekanism:nugget_steel').withChance(8.0),
-		Item.of('mekanism:alloy_infused').withChance(6.0),
-		Item.of('mekanism:basic_control_circuit').withChance(2.0)
+		Item.of('astro_rise:steel_mechanism').withChance(1.0),
+		Item.of('create:powdered_obsidian').withChance(0.06),
+		Item.of('thermal:invar_nugget').withChance(0.08),
+		Item.of('mekanism:nugget_steel').withChance(0.08),
+		Item.of('mekanism:alloy_infused').withChance(0.06),
+		Item.of('mekanism:basic_control_circuit').withChance(0.02)
 	], 'ad_astra:steel_plate', [
 		event.recipes.createDeploying('astro_rise:incomplete_steel_mechanism', [
 			'astro_rise:incomplete_steel_mechanism',
@@ -18,7 +18,7 @@ ServerEvents.recipes(event => {
 		]),
 		event.recipes.createDeploying('astro_rise:incomplete_steel_mechanism', [
 			'astro_rise:incomplete_steel_mechanism',
-			'mechanism:advanced_control_circuit'
+			'mekanism:advanced_control_circuit'
 		])
 	]).transitionalItem('astro_rise:incomplete_steel_mechanism').loops(5)
 
