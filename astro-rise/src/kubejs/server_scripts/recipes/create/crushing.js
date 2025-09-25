f960de7de8e825beebfee9b1a44bfed2b08c06a8
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-25 18:44:17 +0200

    Work on ores processing

--- astro-rise/src/kubejs/server_scripts/recipes/create/crushing.js
+++ astro-rise/src/kubejs/server_scripts/recipes/create/crushing.js
@@ -9,13 +9,13 @@ ServerEvents.recipes(event => {
 	event.remove({ type: 'create:crushing',  input: '#create:stone_types/quartz' })
 
 	Ingredient.of({tag: 'minecraft:flowers'}).getItemIds().forEach(flower => {
-		console.log(`ASTRO_DEBUG : ${flower.toJson()}`)
-	    event.remove({ type: 'create:crushing', input: flower })
+		console.log(`ASTRO_DEBUG : ${flower.getId()}`)
+	    event.remove({ type: 'create:crushing', input: flower.getId() })
 	})
 
 	Ingredient.of({tag: 'createaddition:plants'}).getItemIds().forEach(plant => {
-		console.log(`ASTRO_DEBUG : ${plant.toJson()}`)
-	    event.remove({ type: 'create:crushing', input: plant })
+		console.log(`ASTRO_DEBUG : ${plant.getId()}`)
+	    event.remove({ type: 'create:crushing', input: plant.getId() })
 	})
 
 	event.remove({ type: 'create:crushing',  output: 'createdieselgenerators:wood_chip' })
