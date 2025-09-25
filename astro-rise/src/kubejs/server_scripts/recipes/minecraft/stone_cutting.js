f95e9b1e841bc26b917be5c902ef0f5a1dee8720
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-25 13:07:06 +0200

    Work on ores processing

--- astro-rise/src/kubejs/server_scripts/recipes/minecraft/stone_cutting.js
+++ astro-rise/src/kubejs/server_scripts/recipes/minecraft/stone_cutting.js
@@ -1,23 +1,23 @@
 ServerEvents.recipes(event => {
 
 	event.stonecutting(
-	    Item.of('createdieselgenerators:mold', { tag: { Mold: 'createdieselgenerators:bowl' }}),
+	    Item.of('createdieselgenerators:mold', 1, { nbt: { Mold: 'createdieselgenerators:bowl' }}),
 	    'simplemetals_aluminum:aluminum_sheet'
-	)
+	).id('astro_rise:stonecut/mold_bowl')
 
 	event.stonecutting(
-	    Item.of('createdieselgenerators:mold', { tag: { Mold: 'createdieselgenerators:wavy' }}),
+	    Item.of('createdieselgenerators:mold', 1, { nbt: { Mold: 'createdieselgenerators:wavy' }}),
 	    'simplemetals_aluminum:aluminum_sheet'
-  	)
+  	).id('astro_rise:stonecut/mold_wavy')
 
 	event.stonecutting(
-	    Item.of('createdieselgenerators:mold', { tag: { Mold: 'createdieselgenerators:chain' }}),
+	    Item.of('createdieselgenerators:mold', 1, { nbt: { Mold: 'createdieselgenerators:chain' }}),
 	    'simplemetals_aluminum:aluminum_sheet'
-  	)
+  	).id('astro_rise:stonecut/mold_chain')
 
 	event.stonecutting(
-	    Item.of('createdieselgenerators:mold', { tag: { Mold: 'createdieselgenerators:bar' }}),
+	    Item.of('createdieselgenerators:mold', 1, { nbt: { Mold: 'createdieselgenerators:bar' }}),
 	    'simplemetals_aluminum:aluminum_sheet'
-  	)
+  	).id('astro_rise:stonecut/mold_bar')
 
 })
