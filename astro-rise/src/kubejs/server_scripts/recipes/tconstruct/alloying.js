4a298db4532c6c1a2a581611bb52ec2c4c289d64
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-27 12:42:54 +0200

    Work on ores processing

--- astro-rise/src/kubejs/server_scripts/recipes/tconstruct/alloying.js
+++ astro-rise/src/kubejs/server_scripts/recipes/tconstruct/alloying.js
@@ -2,15 +2,15 @@ ServerEvents.recipes(event => {
 
 	// molten_electrum
 
-	event.recipes.tconstruct.alloy(
-	    { fluid: "tconstruct:molten_electrum", amount: 90 },
-	    [
-	    	{ fluid: "tconstruct:molten_gold", amount: 360 },
-	    	{ fluid: "tconstruct:molten_quartz", amount: 300 },
-	    	{ fluid: "tconstruct:molten_ender", amount: 250 },
-	    	{ fluid: "tconstruct:molten_uranium", amount: 90 }
-	    ],
-	    1500
-	)
+	// event.recipes.tconstruct.alloy(
+	//     { fluid: "tconstruct:molten_electrum", amount: 90 },
+	//     [
+	//     	{ fluid: "tconstruct:molten_gold", amount: 360 },
+	//     	{ fluid: "tconstruct:molten_quartz", amount: 300 },
+	//     	{ fluid: "tconstruct:molten_ender", amount: 250 },
+	//     	{ fluid: "tconstruct:molten_uranium", amount: 90 }
+	//     ],
+	//     1500
+	// )
 
 })
