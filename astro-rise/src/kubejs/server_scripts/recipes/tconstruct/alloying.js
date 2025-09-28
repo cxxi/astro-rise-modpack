27ceb55414c54dc95ac4143f2f4b58a84d23d7a0
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-28 15:28:18 +0200

    Work on ores processing

--- astro-rise/src/kubejs/server_scripts/recipes/tconstruct/alloying.js
+++ astro-rise/src/kubejs/server_scripts/recipes/tconstruct/alloying.js
@@ -29,28 +29,40 @@ ServerEvents.recipes(event => {
 
 	event.remove('tconstruct:smeltery/alloys/molten_signalum')
 
-	// event.recipes.tconstruct.alloy(
-	//     { fluid: "tconstruct:molten_signalum", amount: 360 },
-	//     [
-	//     	{ fluid: "tconstruct:molten_ender", amount: 250 },
-	//     	{ fluid: "tconstruct:molten_ender", amount: 250 },
-	//     	{ fluid: "thermal:redstone", amount: 400 }
-	//     ],
-	//     1350
-	// )
+	event.recipes.tconstruct.alloy(
+	    { fluid: "tconstruct:molten_signalum", amount: 180 },
+	    [
+	    	{ fluid: "astro_rise:molten_calorite", amount: 270 },
+	    	{ fluid: "astro_rise:molten_ostrum", amount: 100 },
+	    	{ fluid: "thermal:redstone", amount: 500 }
+	    ],
+	    1350
+	)
 
 	// molten lumium
 
 	event.remove('tconstruct:smeltery/alloys/molten_lumium')
 
-	// event.recipes.tconstruct.alloy(
-	//     { fluid: "tconstruct:molten_lumium", amount: 360 },
-	//     [
-	//     	{ fluid: "tconstruct:molten_ender", amount: 250 },
-	//     	{ fluid: "tconstruct:molten_ender", amount: 250 },
-	//     	{ fluid: "thermal:glowstone", amount: 400 }
-	//     ],
-	//     1350
-	// )
+	event.recipes.tconstruct.alloy(
+	    { fluid: "tconstruct:molten_lumium", amount: 180 },
+	    [
+	    	{ fluid: "astro_rise:molten_calorite", amount: 270 },
+	    	{ fluid: "astro_rise:molten_aerolyte", amount: 100 },
+	    	{ fluid: "thermal:glowstone", amount: 500 }
+	    ],
+	    1350
+	)
+
+	// molten etrium
+
+	event.recipes.tconstruct.alloy(
+	    { fluid: "astro_rise:molten_etrium", amount: 180 },
+	    [
+	    	{ fluid: "tconstruct:molten_lumium", amount: 270 },
+	    	{ fluid: "astro_rise:molten_fluix", amount: 100 },
+	    	{ fluid: "tconstruct:molten_enderium", amount: 500 }
+	    ],
+	    1350
+	)
 
 })
