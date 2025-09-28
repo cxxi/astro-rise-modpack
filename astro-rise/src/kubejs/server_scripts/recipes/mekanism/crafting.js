8fca5eacc2c56810281ba185cc3a128fef8b7f10
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-09-28 13:38:11 +0200

    Work on ores processing

--- astro-rise/src/kubejs/server_scripts/recipes/mekanism/crafting.js
+++ astro-rise/src/kubejs/server_scripts/recipes/mekanism/crafting.js
@@ -1,5 +1,37 @@
 ServerEvents.recipes(event => {
 
+	// metallurgic_infuser
+
+	event.remove({ id: 'mekanism:metallurgic_infuser' })
+
+	event.shaped('mekanism:metallurgic_infuser', [
+	    'ABA', 
+	    'CDC',
+	    'AEA'  
+	], {
+		A: 'twilightforest:knightmetal_ingot',
+	    B: 'astro_rise:sculk_mechanism',
+	    C: 'createaddition:electrum_spool',
+	    D: 'mekanism:block_osmium',
+	    E: 'createdieselgenerators:burner'
+	})
+
+	// heat_generator
+
+	event.remove({ id: 'mekanismgenerators:generator/heat' })
+
+	event.shaped('mekanismgenerators:heat_generator', [
+		'AAA', 
+	    'BCB',
+	    'DED' 
+	], {
+		A: 'mekanism:enriched_iron',
+		B: '#minecraft:planks',
+		C: 'mekanism:ingot_osmium',
+		D: 'create:copper_sheet',
+		E: 'create:blaze_burner'
+	})
+
 	// basic_fluid_tank
 
 	event.remove({ id: 'mekanism:fluid_tank/basic' })
