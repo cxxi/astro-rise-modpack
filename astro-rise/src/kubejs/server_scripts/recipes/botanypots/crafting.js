6993ab94f6afe7f00606c870f549d9773647999c
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-01 21:46:44 +0200

    DEV - Add molten version of enderio metals

--- astro-rise/src/kubejs/server_scripts/recipes/botanypots/crafting.js
+++ astro-rise/src/kubejs/server_scripts/recipes/botanypots/crafting.js
@@ -19,34 +19,32 @@ ServerEvents.recipes(event => {
 
 	global.COLORS.forEach(color => {
 
+		// basic terracotta
+
 	   	event.shapeless(`botanypots:${color}_terracotta_botany_pot`, [
 	   		'#botanypots:basic_botany_pots',
 	   		`minecraft:${color}_terracotta`
 		])
+
+		// basic concrete
 
 		event.shapeless(`botanypots:${color}_concrete_botany_pot`, [
 			'#botanypots:basic_botany_pots',
 	   		`minecraft:${color}_concrete`
 		])
 
+		// basic glazed terracotta
+
 		event.shapeless(`botanypots:${color}_glazed_terracotta_botany_pot`, [
 			'#botanypots:basic_botany_pots',
 	   		`minecraft:${color}_glazed_terracotta`
 		])
 
+		// hopper terracotta
+
 		event.shapeless(`botanypots:${color}_terracotta_hopper_botany_pot`, [
 	   		'#botanypots:hopper_botany_pots',
 	   		`minecraft:${color}_terracotta`
-		])
-
-		event.shapeless(`botanypots:${color}_concrete_hopper_botany_pot`, [
-			'#botanypots:hopper_botany_pots',
-	   		`minecraft:${color}_concrete`
-		])
-
-		event.shapeless(`botanypots:${color}_glazed_terracotta_hopper_botany_pot`, [
-			'#botanypots:hopper_botany_pots',
-	   		`minecraft:${color}_glazed_terracotta`
 		])
 
 		event.shapeless(`botanypots:${color}_terracotta_hopper_botany_pot`, [
@@ -54,10 +52,24 @@ ServerEvents.recipes(event => {
 	   		'minecraft:hopper'
 		])
 
+		// hopper concrete
+
+		event.shapeless(`botanypots:${color}_concrete_hopper_botany_pot`, [
+			'#botanypots:hopper_botany_pots',
+	   		`minecraft:${color}_concrete`
+		])
+
 		event.shapeless(`botanypots:${color}_concrete_hopper_botany_pot`, [
 			`botanypots:${color}_concrete_botany_pot`,
 	   		'minecraft:hopper'
 		])
+
+		// hopper glazed terracotta
+
+		event.shapeless(`botanypots:${color}_glazed_terracotta_hopper_botany_pot`, [
+			'#botanypots:hopper_botany_pots',
+	   		`minecraft:${color}_glazed_terracotta`
+		])		
 
 		event.shapeless(`botanypots:${color}_glazed_terracotta_hopper_botany_pot`, [
 			`botanypots:${color}_glazed_terracotta_botany_pot`,
