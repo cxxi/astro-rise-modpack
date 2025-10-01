993724d0e078bae2e4858d67c29fc93f8cbc770b
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-01 15:34:23 +0200

    DEV - Add molten version of enderio metals

--- astro-rise/src/kubejs/server_scripts/recipes/tconstruct/casting_table.js
+++ astro-rise/src/kubejs/server_scripts/recipes/tconstruct/casting_table.js
@@ -50,6 +50,240 @@ ServerEvents.recipes(event => {
 	    cooling_time: 100
 	})
 
+	// copper_alloy
+
+	withAllCast(event, {
+	    type: 'tconstruct:casting_table',
+	    fluid: {
+	    	fluid: 'astro_rise:molten_copper_alloy',
+	    	amount: 100
+	    },
+	    result: {
+	    	item: 'enderio:copper_alloy_nugget'
+	    },
+	    cooling_time: 20
+	})
+
+	withAllCast(event, {
+	    type: 'tconstruct:casting_table',
+	    fluid: {
+	    	fluid: 'astro_rise:molten_copper_alloy',
+	    	amount: 900
+	    },
+	    result: {
+	    	item: 'enderio:copper_alloy_ingot'
+	    },
+	    cooling_time: 180
+	})
+
+	// energetic_alloy
+
+	withAllCast(event, {
+	    type: 'tconstruct:casting_table',
+	    fluid: {
+	    	fluid: 'astro_rise:molten_energetic_alloy',
+	    	amount: 100
+	    },
+	    result: {
+	    	item: 'enderio:energetic_alloy_nugget'
+	    },
+	    cooling_time: 20
+	})
+
+	withAllCast(event, {
+	    type: 'tconstruct:casting_table',
+	    fluid: {
+	    	fluid: 'astro_rise:molten_energetic_alloy',
+	    	amount: 900
+	    },
+	    result: {
+	    	item: 'enderio:energetic_alloy_ingot'
+	    },
+	    cooling_time: 180
+	})
+
+	// vibrant_alloy
+
+	withAllCast(event, {
+	    type: 'tconstruct:casting_table',
+	    fluid: {
+	    	fluid: 'astro_rise:molten_vibrant_alloy',
+	    	amount: 100
+	    },
+	    result: {
+	    	item: 'enderio:vibrant_alloy_nugget'
+	    },
+	    cooling_time: 20
+	})
+
+	withAllCast(event, {
+	    type: 'tconstruct:casting_table',
+	    fluid: {
+	    	fluid: 'astro_rise:molten_vibrant_alloy',
+	    	amount: 900
+	    },
+	    result: {
+	    	item: 'enderio:vibrant_alloy_ingot'
+	    },
+	    cooling_time: 180
+	})
+
+	// redstone_alloy
+
+	withAllCast(event, {
+	    type: 'tconstruct:casting_table',
+	    fluid: {
+	    	fluid: 'astro_rise:molten_redstone_alloy',
+	    	amount: 100
+	    },
+	    result: {
+	    	item: 'enderio:redstone_alloy_nugget'
+	    },
+	    cooling_time: 20
+	})
+
+	withAllCast(event, {
+	    type: 'tconstruct:casting_table',
+	    fluid: {
+	    	fluid: 'astro_rise:molten_redstone_alloy',
+	    	amount: 900
+	    },
+	    result: {
+	    	item: 'enderio:redstone_alloy_ingot'
+	    },
+	    cooling_time: 180
+	})
+
+	// conductive_alloy
+
+	withAllCast(event, {
+	    type: 'tconstruct:casting_table',
+	    fluid: {
+	    	fluid: 'astro_rise:molten_conductive_alloy',
+	    	amount: 100
+	    },
+	    result: {
+	    	item: 'enderio:conductive_alloy_nugget'
+	    },
+	    cooling_time: 20
+	})
+
+	withAllCast(event, {
+	    type: 'tconstruct:casting_table',
+	    fluid: {
+	    	fluid: 'astro_rise:molten_conductive_alloy',
+	    	amount: 900
+	    },
+	    result: {
+	    	item: 'enderio:conductive_alloy_ingot'
+	    },
+	    cooling_time: 180
+	})
+
+	// pulsating_alloy
+
+	withAllCast(event, {
+	    type: 'tconstruct:casting_table',
+	    fluid: {
+	    	fluid: 'astro_rise:molten_pulsating_alloy',
+	    	amount: 100
+	    },
+	    result: {
+	    	item: 'enderio:pulsating_alloy_nugget'
+	    },
+	    cooling_time: 20
+	})
+
+	withAllCast(event, {
+	    type: 'tconstruct:casting_table',
+	    fluid: {
+	    	fluid: 'astro_rise:molten_pulsating_alloy',
+	    	amount: 900
+	    },
+	    result: {
+	    	item: 'enderio:pulsating_alloy_ingot'
+	    },
+	    cooling_time: 180
+	})
+
+	// dark_steel
+
+	withAllCast(event, {
+	    type: 'tconstruct:casting_table',
+	    fluid: {
+	    	fluid: 'astro_rise:molten_dark_steel',
+	    	amount: 100
+	    },
+	    result: {
+	    	item: 'enderio:dark_steel_nugget'
+	    },
+	    cooling_time: 20
+	})
+
+	withAllCast(event, {
+	    type: 'tconstruct:casting_table',
+	    fluid: {
+	    	fluid: 'astro_rise:molten_dark_steel',
+	    	amount: 900
+	    },
+	    result: {
+	    	item: 'enderio:dark_steel_ingot'
+	    },
+	    cooling_time: 180
+	})
+
+	// end_steel
+
+	withAllCast(event, {
+	    type: 'tconstruct:casting_table',
+	    fluid: {
+	    	fluid: 'astro_rise:molten_end_steel',
+	    	amount: 100
+	    },
+	    result: {
+	    	item: 'enderio:end_steel_nugget'
+	    },
+	    cooling_time: 20
+	})
+
+	withAllCast(event, {
+	    type: 'tconstruct:casting_table',
+	    fluid: {
+	    	fluid: 'astro_rise:molten_end_steel',
+	    	amount: 900
+	    },
+	    result: {
+	    	item: 'enderio:end_steel_ingot'
+	    },
+	    cooling_time: 180
+	})
+
+	// soularium
+
+	withAllCast(event, {
+	    type: 'tconstruct:casting_table',
+	    fluid: {
+	    	fluid: 'astro_rise:molten_soularium',
+	    	amount: 100
+	    },
+	    result: {
+	    	item: 'enderio:soularium_nugget'
+	    },
+	    cooling_time: 20
+	})
+
+	withAllCast(event, {
+	    type: 'tconstruct:casting_table',
+	    fluid: {
+	    	fluid: 'astro_rise:molten_soularium',
+	    	amount: 900
+	    },
+	    result: {
+	    	item: 'enderio:soularium_ingot'
+	    },
+	    cooling_time: 180
+	})
+
 	// wither_skeleton_skull
 
 	event.custom({
