a90e66024cd7261f22638053d290af5051266a25
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-01 17:44:15 +0200

    DEV - Add molten version of enderio metals

--- astro-rise/src/kubejs/server_scripts/recipes/botanypots/crafting.js
+++ astro-rise/src/kubejs/server_scripts/recipes/botanypots/crafting.js
@@ -1,5 +1,21 @@
 ServerEvents.recipes(event => {
 	
-	// event.remove({ output: /botanypots:.*/ })
+	event.remove({ id: /botanypots:.*/ })
+
+	// terracotta
+
+	global.COLORS.forEach(color => {
+
+		// `${color}_terracotta`
+		// `${color}_concrete`
+		// `${color}_glazed_terracotta`
+
+		// event.remove({ id: `sleeping_bags:${color}_sleeping_bag` })
+
+		// event.shapeless(`sleeping_bags:${color}_sleeping_bag`, [
+		//     '#forge:sleeping_bags',
+		//     `minecraft:${color}_dye`
+		// ])
+	})
 
 })
