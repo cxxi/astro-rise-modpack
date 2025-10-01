0dce10fa61cc5147adb3c94451c79a9dd73e425d
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-01 13:49:36 +0200

    DEV - Add molten version of enderio metals

--- astro-rise/src/kubejs/startup_scripts/fluids/molten_end_steel_alloy.js
+++ astro-rise/src/kubejs/startup_scripts/fluids/molten_end_steel_alloy.js
@@ -1,7 +1,7 @@
 StartupEvents.registry('fluid', event => {
   
-    event.create('astro_rise:end_stone_alloy')
-    	.stillTexture('astro_rise:fluid/end_stone_alloy_still')
-        .flowingTexture('astro_rise:fluid/end_stone_alloy_flowing')
-        .displayName('Molten End Stone Alloy')
+    event.create('astro_rise:end_steel_alloy')
+    	.stillTexture('astro_rise:fluid/end_steel_alloy_still')
+        .flowingTexture('astro_rise:fluid/end_steel_alloy_flowing')
+        .displayName('Molten End Steel Alloy')
 })
