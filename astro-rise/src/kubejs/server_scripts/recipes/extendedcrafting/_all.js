2d64466306fb3e1e777cd6ffcf9c6a6c7d84d2a2
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-02 19:14:42 +0200

    DEV - Add molten version of enderio metals

--- astro-rise/src/kubejs/server_scripts/recipes/extendedcrafting/_all.js
+++ astro-rise/src/kubejs/server_scripts/recipes/extendedcrafting/_all.js
@@ -1,13 +1,5 @@
 ServerEvents.recipes(event => {
 	
-	// event.remove({ output: /extendedcrafting:.*/ })
-
-	event.remove({ id: 'extendedcrafting:black_iron_ingot' })
-
-	event.shapeless('extendedcrafting:black_iron_ingot', [
-		'minecraft:iron_ingot',
-		'twilightforest:knightmetal_ingot',
-   		'enderio:grains_of_infinity'
-	])
+	event.remove({ output: /extendedcrafting:.*/ })
 
 })
---
+++ astro-rise/src/kubejs/server_scripts/recipes/extendedcrafting/crafting.js
@@ -0,0 +1,11 @@
+ServerEvents.recipes(event => {
+	
+	event.remove({ id: 'extendedcrafting:black_iron_ingot' })
+
+	event.shapeless('extendedcrafting:black_iron_ingot', [
+		'minecraft:iron_ingot',
+		'twilightforest:knightmetal_ingot',
+   		'enderio:grains_of_infinity'
+	])
+
+})
