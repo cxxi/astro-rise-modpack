47bdcc7efea6a00437e709223fa8b8b445ad433c
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-02 23:03:30 +0200

    DEV - Add molten version of enderio metals

---
+++ astro-rise/src/kubejs/server_scripts/recipes/extendedcrafting/combination.js
@@ -0,0 +1,24 @@
+ServerEvents.recipes(event => {
+
+	event.custom({
+		type: "extendedcrafting:combination",
+		power_cost: 400000,
+		input: {
+		  item: "minecraft:iron_ingot"
+		},
+		ingredients: [
+			{ item: "minecraft:potato" },
+			{ item: "minecraft:potato" },
+			{ item: "minecraft:potato" },
+			{ item: "minecraft:potato" },
+			{ item: "minecraft:potato" },
+			{ item: "minecraft:potato" },
+			{ item: "minecraft:potato" },
+			{ item: "minecraft:potato" }
+		],
+		result: {
+			id: "minecraft:stone"
+		}
+	})
+
+})
