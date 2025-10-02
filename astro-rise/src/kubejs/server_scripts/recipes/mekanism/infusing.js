68dc9cfe92e25ca60c5665907f80a64666f2da8f
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-02 20:46:28 +0200

    DEV - Add molten version of enderio metals

--- astro-rise/src/kubejs/server_scripts/recipes/mekanism/infusing.js
+++ astro-rise/src/kubejs/server_scripts/recipes/mekanism/infusing.js
@@ -24,7 +24,7 @@ ServerEvents.recipes(event => {
 		type: 'mekanism:metallurgic_infusing',
 		chemicalInput: {
 			amount: 10,
-			tag: 'mekanism:fluorite'
+			infuse_type: 'mekanism:fluorite'
 		},
 		itemInput: {
 			ingredient: {
