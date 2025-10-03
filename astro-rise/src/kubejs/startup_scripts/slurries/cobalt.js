5302633afe2bd3bc169f7c7dd4a0604b7929c926
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-03 19:44:51 +0200

    DEV - Add molten version of enderio metals

--- astro-rise/src/kubejs/startup_scripts/slurries/cobalt.js
+++ astro-rise/src/kubejs/startup_scripts/slurries/cobalt.js
@@ -2,8 +2,16 @@ StartupEvents.registry('mekanism:slurry', event => {
 
 	// event.create('cobalt', 'forge:ores/cobalt').color(0x1133BA)
 
-	event.create('astro_rise:cobalt')
-        .ore('forge:ores/cobalt')
-        .color(0x1133BA)
+	// event.create('astro_rise:cobalt')
+    //     .ore('forge:ores/cobalt')
+    //     .color(0x1133BA)
+
+	event.create('astro_rise:dirty_cobalt')
+		.texture('mekanism:fluid/dirty')
+		.color(0x2D58A5)
+    
+	event.create('astro_rise:clean_cobalt')
+		.texture('mekanism:fluid/clean')
+		.color(0x1133BA)
 
 })
