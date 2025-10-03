bb4efaf3368f90507f063666077138de9cae7b73
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-04 00:33:50 +0200

    DEV - Add molten version of enderio metals

--- astro-rise/src/kubejs/server_scripts/recipes/createaddition/crafting.js
+++ astro-rise/src/kubejs/server_scripts/recipes/createaddition/crafting.js
@@ -1,5 +1,31 @@
 ServerEvents.recipes(event => {
 
+	event.remove({ id: 'createaddition:crafting/small_connector_copper' })
+
+	event.shapeless('3x createaddition:connector', [
+   		'createaddition:copper_rod',
+   		'create:andesite_alloy',
+   		'thermal:cinnabar'
+   	])
+
+   	event.remove({ id: 'createaddition:crafting/large_connector_gold' })
+
+	event.shapeless('2x createaddition:large_connector', [
+   		'createaddition:gold_rod',
+   		'create:andesite_alloy',
+   		'create:andesite_alloy',
+   		'thermal:cinnabar'
+   	])
+
+   	event.remove({ id: 'createaddition:crafting/large_connector_electrum' })
+
+	event.shapeless('2x createaddition:large_connector', [
+   		'createaddition:electrum_rod',
+   		'create:andesite_alloy',
+   		'create:andesite_alloy',
+   		'thermal:cinnabar'
+   	])
+
 	event.remove({ id: 'createaddition:mechanical_crafting/electric_motor' })
 
 	event.recipes.create.mechanical_crafting('createaddition:electric_motor', [
