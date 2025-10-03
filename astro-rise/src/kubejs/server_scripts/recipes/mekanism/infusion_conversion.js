0dc02597fe87b6b608be8d3e0bdcced35f9ca17b
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-03 11:56:22 +0200

    DEV - Add molten version of enderio metals

---
+++ astro-rise/src/kubejs/server_scripts/recipes/mekanism/infusion_conversion.js
@@ -0,0 +1,42 @@
+ServerEvents.recipes(event => {
+
+	event.custom({
+		type: "mekanism:infusion_conversion",
+		input: {
+			ingredient: {
+				tag: "forge:gems/fluorite"
+			}
+		},
+		output: {
+			amount: 10,
+			infuse_type: "astro_rise:fluorite"
+		}
+	})
+
+	event.custom({
+		type: "mekanism:infusion_conversion",
+		input: {
+			ingredient: {
+				tag: "forge:dusts/fluorite"
+			}
+		},
+		output: {
+			amount: 10,
+			infuse_type: "astro_rise:fluorite"
+		}
+	})
+
+	event.custom({
+		type: "mekanism:infusion_conversion",
+		input: {
+			ingredient: {
+				tag: "forge:storage_blocks/fluorite"
+			}
+		},
+		output: {
+			amount: 10,
+			infuse_type: "astro_rise:fluorite"
+		}
+	})
+
+})
