fb36dccc764e1279a85231b786845ba85c283d0e
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-03 21:52:50 +0200

    DEV - Add molten version of enderio metals

---
+++ astro-rise/src/kubejs/startup_scripts/items/jelly_ingot.js
@@ -0,0 +1,11 @@
+.js
+StartupEvents.registry('item', event => {
+
+	event
+		.create('astro_rise:jelly_ingot')
+		.texture('astro_rise:item/jelly_ingot')
+		.displayName('Jelly Ingot')
+	    .tag("forge:ingots")                      
+	    .tag("forge:ingots/jelly_ingot")
+
+})
