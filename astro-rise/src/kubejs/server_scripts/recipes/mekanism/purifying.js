147cb889ca6083e8722a7876089d8f63f281de34
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-04 21:32:28 +0200

    DEV - Add molten version of enderio metals

--- astro-rise/src/kubejs/server_scripts/recipes/mekanism/purifying.js
+++ astro-rise/src/kubejs/server_scripts/recipes/mekanism/purifying.js
@@ -7,96 +7,96 @@ ServerEvents.recipes(event => {
 		'uranium'
 	]
 
-	mekanismOres.forEach(ore => {
+	// mekanismOres.forEach(ore => {
 
-		event.custom({
-			type: 'mekanism:purifying',
-			chemicalInput: {
-				amount: 1,
-				gas: 'mekanism:oxygen'
-			},
-			itemInput: {
-				ingredient: {
-					tag: `forge:ores/${ore}`
-				}
-			},
-			output: {
-				count: 3,
-				item: `astro_rise:clump_${ore}`
-			}
-		})
+	// 	event.custom({
+	// 		type: 'mekanism:purifying',
+	// 		chemicalInput: {
+	// 			amount: 1,
+	// 			gas: 'mekanism:oxygen'
+	// 		},
+	// 		itemInput: {
+	// 			ingredient: {
+	// 				tag: `forge:ores/${ore}`
+	// 			}
+	// 		},
+	// 		output: {
+	// 			count: 3,
+	// 			item: `astro_rise:clump_${ore}`
+	// 		}
+	// 	})
 
-	})
+	// })
 
-	global.CUSTOM_SLURRIES.forEach(slurry => {
+	// global.CUSTOM_SLURRIES.forEach(slurry => {
 
-		event.custom({
-			type: 'mekanism:purifying',
-			chemicalInput: {
-				amount: 1,
-				gas: 'mekanism:oxygen'
-			},
-			itemInput: {
-				ingredient: {
-					tag: `forge:ores/${slurry}`
-				}
-			},
-			output: {
-				count: 3,
-				item: `astro_rise:clump_${slurry}`
-			}
-		})
+	// 	event.custom({
+	// 		type: 'mekanism:purifying',
+	// 		chemicalInput: {
+	// 			amount: 1,
+	// 			gas: 'mekanism:oxygen'
+	// 		},
+	// 		itemInput: {
+	// 			ingredient: {
+	// 				tag: `forge:ores/${slurry}`
+	// 			}
+	// 		},
+	// 		output: {
+	// 			count: 3,
+	// 			item: `astro_rise:clump_${slurry}`
+	// 		}
+	// 	})
 
-		event.custom({
-			type: 'mekanism:purifying',
-			chemicalInput: {
-				amount: 2,
-				gas: 'mekanism:oxygen'
-			},
-			itemInput: {
-				ingredient: {
-					tag: `forge:storage_blocks/raw_${slurry}`
-				}
-			},
-			output: {
-				count: 18,
-				item: `astro_rise:clump_${slurry}`
-			}
-		})
+	// 	event.custom({
+	// 		type: 'mekanism:purifying',
+	// 		chemicalInput: {
+	// 			amount: 2,
+	// 			gas: 'mekanism:oxygen'
+	// 		},
+	// 		itemInput: {
+	// 			ingredient: {
+	// 				tag: `forge:storage_blocks/raw_${slurry}`
+	// 			}
+	// 		},
+	// 		output: {
+	// 			count: 18,
+	// 			item: `astro_rise:clump_${slurry}`
+	// 		}
+	// 	})
 
-		event.custom({
-			type: 'mekanism:purifying',
-			chemicalInput: {
-				amount: 1,
-				gas: 'mekanism:oxygen'
-			},
-			itemInput: {
-				ingredient: {
-					tag: `forge:raw_materials/${slurry}`
-				}
-			},
-			output: {
-				count: 2,
-				item: `astro_rise:clump_${slurry}`
-			}
-		})
+	// 	event.custom({
+	// 		type: 'mekanism:purifying',
+	// 		chemicalInput: {
+	// 			amount: 1,
+	// 			gas: 'mekanism:oxygen'
+	// 		},
+	// 		itemInput: {
+	// 			ingredient: {
+	// 				tag: `forge:raw_materials/${slurry}`
+	// 			}
+	// 		},
+	// 		output: {
+	// 			count: 2,
+	// 			item: `astro_rise:clump_${slurry}`
+	// 		}
+	// 	})
 
-		event.custom({
-			type: 'mekanism:purifying',
-			chemicalInput: {
-				amount: 1,
-				gas: 'mekanism:oxygen'
-			},
-			itemInput: {
-				ingredient: {
-					tag: `mekanism:shards/${slurry}`
-				}
-			},
-			output: {
-				item: `astro_rise:clump_${slurry}`
-			}
-		})
+	// 	event.custom({
+	// 		type: 'mekanism:purifying',
+	// 		chemicalInput: {
+	// 			amount: 1,
+	// 			gas: 'mekanism:oxygen'
+	// 		},
+	// 		itemInput: {
+	// 			ingredient: {
+	// 				tag: `mekanism:shards/${slurry}`
+	// 			}
+	// 		},
+	// 		output: {
+	// 			item: `astro_rise:clump_${slurry}`
+	// 		}
+	// 	})
 
-	})
+	// })
 
 })
