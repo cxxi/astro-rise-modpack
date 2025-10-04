8fbf7238ae1c0f34934fedb54a77ff35005e7dc0
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-04 14:31:56 +0200

    DEV - Add molten version of enderio metals

--- astro-rise/src/kubejs/server_scripts/recipes/mekanism/washing.js
+++ astro-rise/src/kubejs/server_scripts/recipes/mekanism/washing.js
@@ -14,7 +14,7 @@ ServerEvents.recipes(event => {
             },
             fluidInput: {
                 amount: 5,
-                fluid: '#minecraft:water'
+                fluid: 'minecraft:water'
             }
         }
 
