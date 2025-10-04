aa4ef834aebb8faed248d2c37158067130125057
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-04 21:24:33 +0200

    DEV - Add molten version of enderio metals

--- astro-rise/src/kubejs/server_scripts/recipes/mekanism/dissolution.js
+++ astro-rise/src/kubejs/server_scripts/recipes/mekanism/dissolution.js
@@ -1,10 +1,33 @@
 ServerEvents.recipes(event => {
 
-	// event.remove({ id: 'mekanism:processing/copper/slurry/dirty/from_ore' })
-	// event.remove({ id: 'mekanism:processing/iron/slurry/dirty/from_ore' })
-	// event.remove({ id: 'mekanism:processing/gold/slurry/dirty/from_ore' })
+	const mekanismOres = [
+		'tin',
+		'lead',
+		'osmium',
+		'uranium'
+	]
 
-	// event.remove({ type: 'mekanism:dissolution',  })
+	mekanismOres.forEach(ore => {
+
+		event.custom({
+			type: 'mekanism:dissolution',
+			gasInput: {
+				amount: 1,
+				gas: 'mekanism:sulfuric_acid'
+			},
+			itemInput: {
+				ingredient: {
+					tag: `forge:ores/${ore}`
+				}
+			},
+			output: {
+				amount: 1000,
+				chemicalType: 'slurry',
+				slurry: `mekanism:dirty_${ore}`
+			}
+		})
+
+	})
 
 	global.CUSTOM_SLURRIES.forEach(slurry => {
 
@@ -26,42 +49,42 @@ ServerEvents.recipes(event => {
 			}
 		})
 
-		// event.custom({
-		// 	type: 'mekanism:dissolution',
-		// 	gasInput: {
-		// 		amount: 2,
-		// 		gas: 'mekanism:sulfuric_acid'
-		// 	},
-		// 	itemInput: {
-		// 		ingredient: {
-		// 			tag: `forge:storage_blocks/raw_${slurry}`
-		// 		}
-		// 	},
-		// 	output: {
-		// 		amount: 6000,
-		// 		chemicalType: 'slurry',
-		// 		slurry: `astro_rise:dirty_${slurry}`
-		// 	}
-		// })
+		event.custom({
+			type: 'mekanism:dissolution',
+			gasInput: {
+				amount: 2,
+				gas: 'mekanism:sulfuric_acid'
+			},
+			itemInput: {
+				ingredient: {
+					tag: `forge:storage_blocks/raw_${slurry}`
+				}
+			},
+			output: {
+				amount: 6000,
+				chemicalType: 'slurry',
+				slurry: `astro_rise:dirty_${slurry}`
+			}
+		})
 
-		// event.custom({
-		// 	type: 'mekanism:dissolution',
-		// 	gasInput: {
-		// 		amount: 1,
-		// 		gas: 'mekanism:sulfuric_acid'
-		// 	},
-		// 	itemInput: {
-		// 		amount: 3,
-		// 		ingredient: {
-		// 			tag: `forge:raw_materials/${slurry}`
-		// 		}
-		// 	},
-		// 	output: {
-		// 		amount: 2000,
-		// 		chemicalType: 'slurry',
-		// 		slurry: `astro_rise:dirty_${slurry}`
-		// 	}
-		// })
+		event.custom({
+			type: 'mekanism:dissolution',
+			gasInput: {
+				amount: 1,
+				gas: 'mekanism:sulfuric_acid'
+			},
+			itemInput: {
+				amount: 3,
+				ingredient: {
+					tag: `forge:raw_materials/${slurry}`
+				}
+			},
+			output: {
+				amount: 2000,
+				chemicalType: 'slurry',
+				slurry: `astro_rise:dirty_${slurry}`
+			}
+		})
 
 	})
 })
