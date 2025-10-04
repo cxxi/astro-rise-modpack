dc283b7e58e3b1b737cba1a8a40bb44e7ed24e55
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-04 16:51:23 +0200

    DEV - Add molten version of enderio metals

--- astro-rise/src/kubejs/server_scripts/first_join.js
+++ astro-rise/src/kubejs/server_scripts/first_join.js
@@ -25,14 +25,13 @@ PlayerEvents.loggedIn(event => {
 // })
 
 ServerEvents.loaded(event => {
-
     const BuiltInRegistries = Java.loadClass("net.minecraft.core.registries.BuiltInRegistries");
     const MobCategory = Java.loadClass("net.minecraft.world.entity.MobCategory");
     const LootTable = Java.loadClass("net.minecraft.world.level.storage.loot.LootTable");
     
     const lootDataManager = event.server.getLootData();
     const entityTypeRegistry = BuiltInRegistries.ENTITY_TYPE;
-    const entityIds = Array.from(entityTypeRegistry.keySet());
+    const entityIds = entityTypeRegistry.keySet().toArray();
 
     console.log("List of all creature entity types (1 = has loot, 0 = no loot):");
     for (const entityId of entityIds) {
