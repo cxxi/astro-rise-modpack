96c9837b1041c04cd6b44874b8fc9470ae63381b
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-04 15:16:04 +0200

    DEV - Add molten version of enderio metals

--- astro-rise/src/kubejs/server_scripts/recipes/mekanism/dissolution.js
+++ astro-rise/src/kubejs/server_scripts/recipes/mekanism/dissolution.js
@@ -4,7 +4,7 @@ ServerEvents.recipes(event => {
 	// event.remove({ id: 'mekanism:processing/iron/slurry/dirty/from_ore' })
 	// event.remove({ id: 'mekanism:processing/gold/slurry/dirty/from_ore' })
 
-	event.remove({ type: 'mekanism:dissolution' })
+	event.remove({ type: 'mekanism:dissolution',  })
 
 	global.CUSTOM_SLURRIES.forEach(slurry => {
 
@@ -26,42 +26,42 @@ ServerEvents.recipes(event => {
 		// 	}
 		// })
 
-		// event.custom({
-		// 	type: 'mekanism:dissolution',
-		// 	gasInput: {
-		// 		amount: 2,
-		// 		gas: 'mekanism:sulfuric_acid'
-		// 	},
-		// 	itemInput: {
-		// 		ingredient: {
-		// 			tag: `forge:storage_blocks/raw_${slurry}`
-		// 		}
-		// 	},
-		// 	output: {
-		// 		amount: 6000,
-		// 		chemicalType: 'slurry',
-		// 		slurry: `astro_rise:dirty_${slurry}`
-		// 	}
-		// })
-
 		event.custom({
 			type: 'mekanism:dissolution',
 			gasInput: {
-				amount: 1,
+				amount: 2,
 				gas: 'mekanism:sulfuric_acid'
 			},
 			itemInput: {
-				amount: 3,
 				ingredient: {
-					tag: `forge:raw_materials/${slurry}`
+					tag: `forge:storage_blocks/raw_${slurry}`
 				}
 			},
 			output: {
-				amount: 2000,
+				amount: 6000,
 				chemicalType: 'slurry',
 				slurry: `astro_rise:dirty_${slurry}`
 			}
 		})
+
+		// event.custom({
+		// 	type: 'mekanism:dissolution',
+		// 	gasInput: {
+		// 		amount: 1,
+		// 		gas: 'mekanism:sulfuric_acid'
+		// 	},
+		// 	itemInput: {
+		// 		amount: 3,
+		// 		ingredient: {
+		// 			tag: `forge:raw_materials/${slurry}`
+		// 		}
+		// 	},
+		// 	output: {
+		// 		amount: 2000,
+		// 		chemicalType: 'slurry',
+		// 		slurry: `astro_rise:dirty_${slurry}`
+		// 	}
+		// })
 
 	})
 })
