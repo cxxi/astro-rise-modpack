d035d0cd4b5710886aab018fcee3a65d694eceff
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-04 13:38:45 +0200

    DEV - Add molten version of enderio metals

--- astro-rise/devtool/run-server.sh
+++ astro-rise/devtool/run-server.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 cd ./server || exit 1
-screen -dmS mc bash ./run.sh nogui
-screen -r mc
+
+# Nom de la session tmux
+SESSION_NAME="mc"
+
+# Vérifie si une session du même nom existe déjà
+if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
+  echo "➡️  Session tmux '$SESSION_NAME' déjà en cours. Connexion..."
+else
+  echo "🚀 Démarrage du serveur Minecraft dans une nouvelle session tmux..."
+  tmux new-session -d -s "$SESSION_NAME" "bash ./run.sh nogui"
+fi
+
+# Attache-toi à la session
+tmux attach -t "$SESSION_NAME"
