59c247e77042528c86531b86f16d194ac863f1fb
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-04 21:34:07 +0200

    DEV - Add molten version of enderio metals

--- astro-rise/src/kubejs/server_scripts/recipes/mekanism/injecting.js
+++ astro-rise/src/kubejs/server_scripts/recipes/mekanism/injecting.js
@@ -9,97 +9,97 @@ ServerEvents.recipes(event => {
 		'uranium'
 	]
 
-	// mekanismOres.forEach(ore => {
+	mekanismOres.forEach(ore => {
 
-	// 	event.custom({
-	// 		type: 'mekanism:injecting',
-	// 		chemicalInput: {
-	// 			amount: 1,
-	// 			gas: 'mekanism:hydrogen_chloride'
-	// 		},
-	// 		itemInput: {
-	// 			ingredient: {
-	// 				tag: `forge:ores/${ore}`
-	// 			}
-	// 		},
-	// 		output: {
-	// 			count: 4,
-	// 			item: `astro_rise:shard_${ore}`
-	// 		}
-	// 	})
+		event.custom({
+			type: 'mekanism:injecting',
+			chemicalInput: {
+				amount: 1,
+				gas: 'mekanism:hydrogen_chloride'
+			},
+			itemInput: {
+				ingredient: {
+					tag: `forge:ores/${ore}`
+				}
+			},
+			output: {
+				count: 4,
+				item: `astro_rise:shard_${ore}`
+			}
+		})
 
-	// })
+	})
 
-	// global.CUSTOM_SLURRIES.forEach(slurry => {
+	global.CUSTOM_SLURRIES.forEach(slurry => {
 
-	// 	event.custom({
-	// 		type: 'mekanism:injecting',
-	// 		chemicalInput: {
-	// 			amount: 1,
-	// 			gas: 'mekanism:hydrogen_chloride'
-	// 		},
-	// 		itemInput: {
-	// 			ingredient: {
-	// 				tag: `mekanism:crystals/${slurry}`
-	// 			}
-	// 		},
-	// 		output: {
-	// 			item: `astro_rise:shard_${slurry}`
-	// 		}
-	// 	})
+		event.custom({
+			type: 'mekanism:injecting',
+			chemicalInput: {
+				amount: 1,
+				gas: 'mekanism:hydrogen_chloride'
+			},
+			itemInput: {
+				ingredient: {
+					tag: `mekanism:crystals/${slurry}`
+				}
+			},
+			output: {
+				item: `astro_rise:shard_${slurry}`
+			}
+		})
 
-	// 	event.custom({
-	// 		type: 'mekanism:injecting',
-	// 		chemicalInput: {
-	// 			amount: 1,
-	// 			gas: 'mekanism:hydrogen_chloride'
-	// 		},
-	// 		itemInput: {
-	// 			ingredient: {
-	// 				tag: `forge:ores/${slurry}`
-	// 			}
-	// 		},
-	// 		output: {
-	// 			count: 4,
-	// 			item: `astro_rise:shard_${slurry}`
-	// 		}
-	// 	})
+		event.custom({
+			type: 'mekanism:injecting',
+			chemicalInput: {
+				amount: 1,
+				gas: 'mekanism:hydrogen_chloride'
+			},
+			itemInput: {
+				ingredient: {
+					tag: `forge:ores/${slurry}`
+				}
+			},
+			output: {
+				count: 4,
+				item: `astro_rise:shard_${slurry}`
+			}
+		})
 
-	// 	event.custom({
-	// 		type: 'mekanism:injecting',
-	// 		chemicalInput: {
-	// 			amount: 2,
-	// 			gas: 'mekanism:hydrogen_chloride'
-	// 		},
-	// 		itemInput: {
-	// 			ingredient: {
-	// 				tag: `forge:storage_blocks/raw_${slurry}`
-	// 			}
-	// 		},
-	// 		output: {
-	// 			count: 24,
-	// 			item: `astro_rise:shard_${slurry}`
-	// 		}
-	// 	})
+		event.custom({
+			type: 'mekanism:injecting',
+			chemicalInput: {
+				amount: 2,
+				gas: 'mekanism:hydrogen_chloride'
+			},
+			itemInput: {
+				ingredient: {
+					tag: `forge:storage_blocks/raw_${slurry}`
+				}
+			},
+			output: {
+				count: 24,
+				item: `astro_rise:shard_${slurry}`
+			}
+		})
 
-	// 	event.custom({
-	// 		type: 'mekanism:injecting',
-	// 		chemicalInput: {
-	// 			amount: 1,
-	// 			gas: 'mekanism:hydrogen_chloride'
-	// 		},
-	// 		itemInput: {
-	// 			amount: 3,
-	// 			ingredient: {
-	// 				tag: `forge:raw_materials/${slurry}`
-	// 			}
-	// 		},
-	// 		output: {
-	// 			count: 8,
-	// 			item: `astro_rise:shard_${slurry}`
-	// 		}
-	// 	})
+		event.custom({
+			type: 'mekanism:injecting',
+			chemicalInput: {
+				amount: 1,
+				gas: 'mekanism:hydrogen_chloride'
+			},
+			itemInput: {
+				amount: 3,
+				ingredient: {
+					tag: `forge:raw_materials/${slurry}`
+				}
+			},
+			output: {
+				count: 8,
+				item: `astro_rise:shard_${slurry}`
+			}
+		})
 
-	// })
+	})
 
 })
