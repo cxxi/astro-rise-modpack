18616aeaace7fabf4dc6e1565f815b8ae294f2f1
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-06 11:41:58 +0200

    DEV - Add molten version of enderio metals

--- astro-rise/src/kubejs/server_scripts/recipes/createaddition/rolling.js
+++ astro-rise/src/kubejs/server_scripts/recipes/createaddition/rolling.js
@@ -1,24 +1,26 @@
 ServerEvents.recipes(event => {
 
+	// half_base_component
+	
 	event.custom({
 		type: 'createaddition:rolling',
 		input: {
 	      	item: 'extendedcrafting:black_iron_slate'
 		},
 		result: {
-			item: 'astro_rise:half_base_component',
-			count: 2
+			item: 'astro_rise:half_base_component'
 		}
 	})
 
+	// base_component
+	
 	event.custom({
 		type: 'createaddition:rolling',
 		input: {
 	      	item: 'astro_rise:half_base_component'
 		},
 		result: {
-			item: 'astro_rise:base_component',
-			count: 2
+			item: 'astro_rise:base_component'
 		}
 	})
 })
--- astro-rise/src/kubejs/startup_scripts/constants.js
+++ astro-rise/src/kubejs/startup_scripts/constants.js
@@ -18,52 +18,101 @@ global.COLORS = [
 	'pink'
 ]
 
-global.ORES = [
-	['minecraft', 'iron'],
-	['minecraft', 'copper'],
-	['minecraft', 'gold'],
-	['create', 'zinc'],
-	['thermal', 'silver'],
-	['thermal', 'nickel'],
-	['mekanism', 'tin'],
-	['mekanism', 'lead'],
-	['mekanism', 'osmium'],
-	['mekanism', 'uranium'],
-	['simplemetals_aluminum', 'aluminum'],
-	['platinum_mod', 'platinum'],
-	['ad_astra', 'desh'],
-	['ad_astra', 'ostrum'],
-	['ad_astra', 'calorite'],
-	['astro_rise', 'aerolyte'],
-	['tconstruct', 'cobalt']
+global.VANILLA_ORES = [
+	'iron',
+	'copper',
+	'gold'
 ]
 
-global.ORE_GEMS = [
-	['minecraft', 'coal', 0],
-	['minecraft', 'redstone', 1],
-	['minecraft', 'lapis_lazuli', 1],
-	['minecraft', 'quartz', 1],
-	['minecraft', 'emerald', 2],
-	['minecraft', 'diamond', 2],
-	['mekanism', 'fluorite_gem', 2],
-	['thermal', 'apatite', 2],
-	['thermal', 'cinnabar', 2],
-	['thermal', 'sapphire', 2],
-	['thermal', 'ruby', 2],
-	['thermal', 'niter', 2],
-	['thermal', 'sulfur', 2],
-	['ad_astra', 'ice_shard', 2]
+global.CREATE_ORES = [
+	'create'
+]
+
+global.MEKANISM_ORES = [
+	'tin',
+	'lead',
+	'osmium',
+	'uranium'
+]
+
+global.THERMAL_ORES = [
+	'silver',
+	'nickel'
+]
+
+global.SIMPLEMETALS_ORES = [
+	'aluminum'
+]
+
+global.PLATINUM_ORES = [
+	'platinum'
+]
+
+global.TCONSTRUCT_ORES = [
+	'cobalt'
+]
+
+global.AD_ASTRA_ORES = [
+	'desh',
+	'ostrum',
+	'calorite'
+]
+
+global.ASTRO_RISE_ORES = [
+	'aerolyte'
+]
+
+global.ORES = [
+	...global.VANILLA_ORES.map(ore => ['minecraft', ore]),
+	...global.CREATE_ORES.map(ore => ['create', ore]),
+	...global.MEKANISM_ORES.map(ore => ['mekanism', ore]),
+	...global.THERMAL_ORES.map(ore => ['thermal', ore]),
+	...global.SIMPLEMETALS_ORES.map(ore => ['simplemetals_aluminum', ore]),
+	...global.PLATINUM_ORES.map(ore => ['platinum_mod', ore]),
+	...global.TCONSTRUCT_ORES.map(ore => ['tconstruct', ore]),
+	...global.AD_ASTRA_ORES.map(ore => ['ad_astra', ore]),
+	...global.ASTRO_RISE_ORES.map(ore => ['astro_rise', ore])
 ]
 
 global.CUSTOM_SLURRIES = [
-	'aerolyte',
-	'aluminum',
-	'calorite',
-	'cobalt',
-	'desh',
-	'nickel',
-	'ostrum',
-	'platinum',
-	'silver',
-	'zinc'
+	...global.SIMPLEMETALS_ORES,
+	...global.PLATINUM_ORES,
+	...global.TCONSTRUCT_ORES,
+	...global.AD_ASTRA_ORES,
+	...global.ASTRO_RISE_ORES,
+	...global.THERMAL_ORES,
+	...global.CREATE_ORES
 ]
+
+// global.ORES = [
+// 	['minecraft', 'iron'],
+// 	['minecraft', 'copper'],
+// 	['minecraft', 'gold'],
+// 	['create', 'zinc'],
+// 	['thermal', 'silver'],
+// 	['thermal', 'nickel'],
+// 	['mekanism', 'tin'],
+// 	['mekanism', 'lead'],
+// 	['mekanism', 'osmium'],
+// 	['mekanism', 'uranium'],
+// 	['simplemetals_aluminum', 'aluminum'],
+// 	['platinum_mod', 'platinum'],
+// 	['ad_astra', 'desh'],
+// 	['ad_astra', 'ostrum'],
+// 	['ad_astra', 'calorite'],
+// 	['astro_rise', 'aerolyte'],
+// 	['tconstruct', 'cobalt']
+// ]
+
+// global.CUSTOM_SLURRIES = [
+// 	'aerolyte',
+// 	'aluminum',
+// 	'calorite',
+// 	'cobalt',
+// 	'desh',
+// 	'nickel',
+// 	'ostrum',
+// 	'platinum',
+// 	'silver',
+// 	'zinc'
+// ]
