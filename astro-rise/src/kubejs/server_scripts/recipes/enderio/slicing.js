74b477fce026fe957f0e64e7b366d45f39ca9657
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-06 23:50:27 +0200

    DEV - Update recipe

--- astro-rise/src/kubejs/server_scripts/recipes/enderio/slicing.js
+++ astro-rise/src/kubejs/server_scripts/recipes/enderio/slicing.js
@@ -1,16 +1,5 @@
 ServerEvents.recipes(event => {
 
-	// z_logic_controller
-
-	event.remove({ id: "enderio:slicing/z_logic_controller" })
-
-	event.recipes.enderio.slicing("enderio:z_logic_controller", [
-		"enderio:soularium_ingot",
-		"minecraft:zombie_head",
-		"enderio:soularium_ingot",
-		"enderio:nethercotta",
-		"minecraft:redstone",
-		"enderio:nethercotta"
-	])
+	event.remove({ type: 'enderio:slicing' })
 
 })
