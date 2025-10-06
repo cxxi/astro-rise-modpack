a853f524db5029cea00b57003c4a082d12cbe206
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-06 10:19:55 +0200

    DEV - Add molten version of enderio metals

--- astro-rise/src/kubejs/server_scripts/recipes/createaddition/rolling.js
+++ astro-rise/src/kubejs/server_scripts/recipes/createaddition/rolling.js
@@ -1,5 +1,24 @@
 ServerEvents.recipes(event => {
 
-	// 'astro_rise:base_component'
-	// 'extendedcrafting:black_iron_slate'
+	event.custom({
+		type: 'createaddition:rolling',
+		input: {
+	      	item: 'extendedcrafting:black_iron_slate'
+		},
+		result: {
+			item: 'astro_rise:half_base_component',
+			count: 2
+		}
+	})
+
+	event.custom({
+		type: 'createaddition:rolling',
+		input: {
+	      	item: 'astro_rise:half_base_component'
+		},
+		result: {
+			item: 'astro_rise:base_component',
+			count: 2
+		}
+	})
 })
---
+++ astro-rise/src/kubejs/startup_scripts/items/half_base_component.js
@@ -0,0 +1,8 @@
+StartupEvents.registry('item', event => {
+
+	event
+		.create('astro_rise:half_base_component')
+		.texture('astro_rise:item/half_base_component')
+		.displayName('Half Base Component')
+
+})
