028ac8d66344384d10b5569ac0cfb6a8ba7fe6ce
Louis-Antoine Lumet <l.a_lumet@yahoo.fr>
2025-10-12 02:34:46 +0200

    DEV - Update recipe

--- astro-rise/src/kubejs/server_scripts/tags/items/gears.js
+++ astro-rise/src/kubejs/server_scripts/tags/items/gears.js
@@ -8,6 +8,16 @@ ServerEvents.tags('item', event => {
 		'enderio:vibrant_gear',
 		'enderio:energized_gear'
 	])
-
 })
 
+ServerEvents.tags('items', event => {
+
+	event.remove('forge:gears', [
+		'enderio:wood_gear',
+		'enderio:stone_gear',
+		'enderio:iron_gear',
+		'enderio:dark_bimetal_gear',
+		'enderio:vibrant_gear',
+		'enderio:energized_gear'
+	])
+})
